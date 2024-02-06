5c12b55dc394b8e51d2e4d8337b5a37c07d787e3
Adeel Khan <mak000@gmail.com>
2024-02-07 02:59:02 +0500

    initial commit. Implements;
    signin, create, update, list functions

---
+++ app.py
@@ -0,0 +1,138 @@
+from flask import Flask, request, jsonify
+from datetime import datetime
+import uuid 
+app = Flask(__name__)
+
+
+# model
+class TodoItem:
+  def __init__(self, id, item_name, created_time_stamp, updated_time_stamp):
+    self.id = id 
+    self.item_name = item_name 
+    self.created_time_stamp = created_time_stamp
+    self.updated_time_stamp = updated_time_stamp
+
+class UserProfile:
+  def __init__(self, username, password):
+    self.username = username 
+    self.password = password 
+    self.todoItem = dict()
+
+
+
+# structures
+todoMap = {}
+
+users = {
+  "user1@abc.com":UserProfile("user1@abc.com", "password1"),
+  "user2@abc.com":UserProfile("user2@abc.com", "password2"),
+}
+
+# helpers
+def get_user_profile(request_json):
+  username = request_json['username']
+  password = request_json['password']
+  if username not in users:
+    return None, "Invalid credentials, requested user not found"
+
+  if users[username].username == username and \
+    users[username].password == password:
+    return users[username], ""
+
+
+
+# handlers
+@app.route('/')
+def hello():
+  return 'hello world'
+
+@app.route('/signin', methods=["POST"])
+def Signin():
+  request_json = request.get_json()
+  user, error = get_user_profile(request_json)
+  if user:
+    return {
+      "Msg": "Success",
+      "Status": 200,
+      "User": user.username,
+    }
+  else:
+    return {
+      "Msg": error,
+      "Status": 401,
+    }
+    
+
+@app.route('/refresh', methods=["POST"])
+def Refresh():
+  pass 
+
+@app.route('/create', methods=["POST"])
+def Create():
+  request_json = request.get_json()
+  item_name = request_json['item_name']
+  uu_id = uuid.uuid4().hex
+  create_time = datetime.now().ctime()
+  todoMap[uu_id] = TodoItem(uu_id, item_name, create_time, create_time)
+  users['user1@abc.com'].todoItem[uu_id]=uu_id
+
+  return {
+      "Msg": "Success",
+      "Status": 201,
+  }
+
+@app.route('/update', methods=["POST"])
+def Update():
+  request_json = request.get_json()
+  item_id = request_json['item_id']
+  item_name = request_json['item_name']
+
+  # search and update 
+  for uuid, _ in todoMap.items():
+    if uuid == item_id:
+      todoMap[uuid].item_name = item_name
+      todoMap[uuid].update_time_stamp = datetime.now().ctime()
+      return {
+        "Msg": "Success",
+        "Status": 200,
+      } 
+  return {
+      "Error": "Bad request check payload",
+      "Status": 401,
+  } 
+
+@app.route('/delete', methods=["POST"])
+def Delete():
+  user_profile = users['user1@abc.com']
+  todo_list = user_profile.todoItem
+
+  request_json = request.get_json()
+
+  item_id = request_json['item_id']
+  todo_list.remove(item_id)
+  todoMap.pop(item_id, None)
+
+  return {
+    "Msg": "Success",
+    "Status": 200,
+  }
+
+@app.route('/list', methods=["GET"])
+def List():
+  user_profile = users['user1@abc.com']
+  todo_list = user_profile.todoItem
+  items = []
+  for id in todo_list:
+    todo_item = todoMap[id]
+    items.append({
+      "id": todo_item.id,
+      "item_name": todo_item.item_name,
+      "created_time_stamp": todo_item.created_time_stamp,
+      "updated_time_stamp": todo_item.updated_time_stamp
+    })
+    
+  return {
+    "Msg": "Success",
+    "Status": 200,
+    "Items": items
+  }
